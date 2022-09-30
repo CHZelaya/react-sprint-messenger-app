3dbe4eb71443329a301d3b43c77f143dc0004a45
CHZelaya <chzelaya1201@gmail.com>
2022-09-30 13:04:20 -0600

    #1 Created base skeletons of components

---
+++ src/components/login/login.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from "../button/button.component"
+
+const Login = () => {
+    return (
+        <div className="login-container">
+            <h3> This is the login component</h3>
+            <Button> Login here </Button>
+        </div>
+    )
+}
+
+export default Login
